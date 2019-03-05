0783ce2c947d19a16cf6760f2688b42ab0f58a12
nagenn <nvmurti@netscape.net>
2019-03-05 18:43:14 +0530

    edited for the class

--- Py-Google-HW/helloworld.py
+++ Py-Google-HW/helloworld.py
@@ -1,2 +1,2 @@
 if __name__ == '__main__':
-    print('Hello World - Google via Eclipse - edited on Mar 2 2019 4 pm pn Mac')
+    print('Hello World - Google via Eclipse - edited on Mar 5th at SkillV pn Mac')
