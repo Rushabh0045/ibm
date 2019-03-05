a4bab588a93790beae07abb8b14bd93c9f456720
nagenn <nvmurti@netscape.net>
2019-03-05 19:06:37 +0530

    Simple change 3

--- Py-Google-HW/helloworld.py
+++ Py-Google-HW/helloworld.py
@@ -1,3 +1,3 @@
 
 if __name__ == '__main__':
-    print('Hello World - Google via Eclipse - edited on Mar 5 2019 7 pm at SkillVelo pn Mac')
+    print('Hello World - Google via Eclipse - edited on Mar 5 2019 7:06 pm at JP Nagar pn Mac')
