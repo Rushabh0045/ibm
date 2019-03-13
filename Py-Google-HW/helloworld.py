7f3568b567cfde1bd133dbe81dba2c6c830e5d4b
nagenn <44678680+nagenn@users.noreply.github.com>
2019-03-13 23:28:02 +0530

    Update helloworld.py

--- Py-Google-HW/helloworld.py
+++ Py-Google-HW/helloworld.py
@@ -1,3 +1,3 @@
 
 if __name__ == '__main__':
-    print('Hello World - Google via Eclipse - edited for Devops class - try PR')
+    print('Hello World - Google via Eclipse - edited for Devops class - try ngrok')
